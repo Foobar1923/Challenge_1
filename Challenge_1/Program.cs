43afca081aab2fb188a4f759c916a9c56c1f5eda
Dejuve <roostertooth2319@gmail.com>
2019-01-16 18:16:34 -0600

    Final Project

---
+++ Challenge_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+                your name is (blank), you are (blank) years old, and your username is (blank)
+             */
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            string userAge = Console.ReadLine();
+            int intAge = Convert.ToInt32(userAge);
+            Console.WriteLine("Please enter your Reddit username: ");
+            string redditName = Console.ReadLine();
+
+            Console.WriteLine("Your name is {0}, your age is {1} and your Reddit username is {2}.", userName, intAge, redditName);
+        }
+    }
+}
