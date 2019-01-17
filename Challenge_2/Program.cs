26d14b687580f1f080dbea615a3458905dd0fd7e
Dejuve <roostertooth2319@gmail.com>
2019-01-16 18:22:02 -0600

    commit 1

---
+++ Challenge_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Challenge_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                create a calculator application that has use in your life. 
+                It might be an interest calculator, or it might be something that you can use in the classroom. 
+                For example, if you were in physics class, you might want to make a F = M * A calc.
+
+                Temperature Converter - F <-> C <-> K
+
+             */
+            
+
+        }
+    }
+}
