06ce2810cb764163569443e42260e4a4bf73689b
Foobar <beardsyndicate.tv@gmail.com>
2019-01-31 08:49:09 -0600

    restart

--- Challenge_3/Program.cs
+++ Challenge_3/Program.cs
@@ -13,39 +13,7 @@ namespace Challenge_3
             write a program that can encrypt texts with an alphabetical caesar cipher. This cipher can ignore numbers, symbols, and whitespace.            
             cipher - shift each letter 1 to the left or right
             */
-             
-            string phrase; 
-            int cipherShift;
-            char[] phraseArray;
-            char[] encryptedArray = {};
-            char[] key = {'a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-            Console.WriteLine("Enter a phrase to encrypt (Numbers, Symbols and Spaces will be ignored):");
-            phrase = Console.ReadLine();
-            phraseArray = phrase.ToCharArray();
-            Console.WriteLine("Enter the number to shift:");
-            cipherShift = Convert.ToInt32(Console.ReadLine());
-            Array.Resize(ref encryptedArray, phrase.Length); // change array size to length of phrase
-            for (int i = 0; i < phrase.Length; i++)
-            {
-                int temp = Array.IndexOf(phraseArray,phraseArray[i]);
-                Console.WriteLine("Temp: {0}", temp);
-                for(int x = 0; x < key.Length; x++)
-                {
-                    if(temp > key[x])
-                    {
-                        int overFlow = (temp % i) + cipherShift;
-                        encryptedArray[i] = key[overFlow];
-                    }
-                    else
-                        if(phraseArray[i] == key[x])
-                        {
-                            encryptedArray[i] = key[x + cipherShift];
-                            Console.WriteLine("Encrypted phrase: {0}", encryptedArray[i]);
-                        }
-                }
-            }
-            //phrase = Convert.ToString(encryptedArray);
-            //Console.WriteLine("Encrypted phrase: {0}", phrase);
+                        
         }
     }
 }
