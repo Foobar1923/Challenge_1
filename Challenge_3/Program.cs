04ffee3898d51b609708a0b216f2e55ed6985cf2
Foobar <beardsyndicate.tv@gmail.com>
2019-01-31 12:42:18 -0600

    .

--- Challenge_3/Program.cs
+++ Challenge_3/Program.cs
@@ -18,19 +18,17 @@ namespace Challenge_3
             string encryptedPhrase = "";
             int cipherShift;
             char[] key = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-            char[] phraseChar = {};
             Console.WriteLine("Enter a phrase to encode (Capital letters, symbols and spaces will be ignored): ");
             phrase = Console.ReadLine();
-            phraseChar = phrase.ToCharArray();
             Console.WriteLine("Please enter the shift number: ");
             cipherShift = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i < phrase.Length; i++) // checks phrase value
+            for(int i = 0; i < phrase.Length; i++)
             {
-                for(int x = 0; x < key.Length; x++) // checks key value
+                for(int x = 0; x < key.Length; x++)
                 {
-                    if(phraseChar[i] == key[x])
+                    if(phrase[i + cipherShift] < key.Length)
                     {
-                        encryptedPhrase += key[x]; // creates encrypted phrase based off current phrase + shifted key value
+                        encryptedPhrase += encryptedPhrase.Append(key[x + cipherShift]);
                     }
                 }
             }
