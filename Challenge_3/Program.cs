b10f8e087d4025474f4f864254eda01a87248065
Foobar <beardsyndicate.tv@gmail.com>
2019-01-31 09:17:30 -0600

    .

--- Challenge_3/Program.cs
+++ Challenge_3/Program.cs
@@ -13,7 +13,29 @@ namespace Challenge_3
             write a program that can encrypt texts with an alphabetical caesar cipher. This cipher can ignore numbers, symbols, and whitespace.            
             cipher - shift each letter 1 to the left or right
             */
-                        
+
+            string phrase;
+            string encryptedPhrase = "";
+            int cipherShift;
+            char[] key = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+            char[] phraseChar = {};
+            Console.WriteLine("Enter a phrase to encode (Capital letters, symbols and spaces will be ignored): ");
+            phrase = Console.ReadLine();
+            phraseChar = phrase.ToCharArray();
+            Console.WriteLine("Please enter the shift number: ");
+            cipherShift = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < phrase.Length; i++) // checks phrase value
+            {
+                for(int x = 0; x < key.Length; x++) // checks key value
+                {
+                    if(phraseChar[i] == key[x])
+                    {
+                        encryptedPhrase += key[x]; // creates encrypted phrase based off current phrase + shifted key value
+                    }
+                }
+            }
+            Console.WriteLine("Test Phrase: {0}", encryptedPhrase);
+
         }
     }
 }
